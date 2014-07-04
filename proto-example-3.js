1b5ee3eaadb58b5e831e441b34b380db7cb8f0a0
Hrishikesh Mishra <sprt.email@gmail.com>
2014-07-04 21:00:09 +0530

    Create proto-example-3.js

---
+++ proto-example-3.js
@@ -0,0 +1,31 @@
+/**
+ * functional inheritance with 
+ * shared secrets 
+ */
+
+function gizmo(id, secret) {
+    var secret = secret || {};
+    secret.id = id;
+    return {
+        toString: function() {
+            return "gizmo " + secret.id;
+        }
+    };
+}
+
+function hoozit(id) {
+    var secret = {};
+    var that = gizmo(id, secret);
+    that.test = function(testid) {
+        return testid = secret.id;
+    };
+    return that;
+}
+
+
+var h = new Hoozit(100);
+console.log("type of = " + (typeof h));
+console.log("h.id = " + h.id);
+console.log("g tostring = " + h.toString());
+console.log("h true test =  " + h.test(100));
+console.log("h false test = " + h.test(999));
