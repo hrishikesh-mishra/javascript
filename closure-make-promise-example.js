12d3f6b0d4e51b1baa3fc995e89dd7a5ae50ae10
Hrishikesh Mishra <sprt.email@gmail.com>
2014-07-04 18:36:24 +0530

    Create closure-make-promise-example.js

---
+++ closure-make-promise-example.js
@@ -0,0 +1,55 @@
+function make_promise() {
+    var status = 'unresolved',
+            outcome,
+            waiting = [],
+            dreading = [];
+
+    function vouch(deed, func) {
+        switch (status) {
+            case 'unresolved' :
+                (deed === 'fulfilled' ? waiting : dreading).push(func);
+                break;
+            case deed:
+                func(outcome);
+                break;
+        }
+    }
+
+    function resolve(deed, value) {
+        if (status !== 'unresolved') {
+            throw new Error(
+                    'The promise has already been resolved : ' + status);
+        }
+        status = deed;
+        outcome = value;
+        (deed === 'fulfiled' ? waiting : dreadin)
+                .forEach(function(func) {
+                    try {
+                        func(outcome);
+                    } catch (ignore) {
+                    }
+
+                });
+        waiting = null;
+        dreading = null;
+    }
+
+    return {
+        when: function(func) {
+            vouch('fulfilled', func);
+        },
+        fail: function(func) {
+            vouch('smashed', func);
+        },
+        fulfill: function(value) {
+            resolve('fulfilled', value);
+        },
+        smash: function(string) {
+            resolve('smash', string)
+        },
+        status: function() {
+            return status;
+        }
+
+    }
+}
