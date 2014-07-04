eee40d952b309fc2831ad60b26be4b6e666641b8
Hrishikesh Mishra <sprt.email@gmail.com>
2014-07-04 20:51:43 +0530

    Create proto-example-1.js

---
+++ proto-example-1.js
@@ -0,0 +1,17 @@
+function Gizmo (id) { 
+    this.id = id; 
+}
+
+Gizmo.prototype.toString = function () { 
+    return "gizmo" + this.id;
+};
+
+function Hoozit (id)  { 
+    this.id = id; 
+}
+
+Hoozit.prototype = new Gizmo();
+Hoozit.prototype.test = function(id) { 
+    return this.id == id;
+};
+
